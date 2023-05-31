e1af07de8ddb76b0e9f2a790307192cf6de9589a
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-05-31 17:55:29 -0300

    WIP: Adicionando teste de validação de presença

--- spec/system/type/user_create_type_spec.rb
+++ spec/system/type/user_create_type_spec.rb
@@ -15,5 +15,21 @@ describe 'usuário cria novo tipo de todo list' do
 
     expect(page).to have_content 'Tipo salvo com sucesso.'
   end
+  it 'com falha' do
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+
+    login_as user
+    visit root_path
+
+    click_on 'Novo tipo'
+
+    fill_in 'Nome', with: ''
+    fill_in 'Descrição', with: ''
+    click_on 'Salvar'
+
+    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'Descrição não pode ficar em branco'
+    expect(current_page).to eq new_type_path
+  end
 end
 
