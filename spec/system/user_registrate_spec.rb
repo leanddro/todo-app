5f30cf489227cfeea8844c39f36faa4269c632bc
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-05-31 16:52:32 -0300

    Resolução do registro do usuario + novo teste para exibição de erros

--- spec/system/user_registrate_spec.rb
+++ spec/system/user_registrate_spec.rb
@@ -16,5 +16,21 @@ describe 'usuário se registrar no sistema' do
     click_on 'Inscrever-se'
 
     expect(page).to have_content 'joao@email.com'
+    expect(page).not_to have_link 'Entrar'
+  end
+
+  it 'com falha' do
+    visit root_path
+    within 'nav' do
+      click_on 'Entrar'
+    end
+
+    fill_in 'Nome', with: ''
+    fill_in 'E-mail', with: 'joao@email.com'
+    fill_in 'Senha', with: ''
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Inscrever-se'
+
+    expect(page).to have_content "Não foi possível salvar usuário: 2 erros."
   end
 end
