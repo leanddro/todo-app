d85f8a0cbe623a600fd64e1ae690682a697f5c81
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-05-31 16:27:14 -0300

    Filtrando log de teste

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,13 @@ RSpec.configure do |config|
   # https://rspec.info/features/6-0/rspec-rails
   config.infer_spec_type_from_file_location!
 
+  config.backtrace_exclusion_patterns = [
+    /\/lib\d*\/ruby\//,
+    /bin\//,
+    /gems/,
+    /spec\/spec_helper\.rb/,
+    /lib\/rspec\/(core|expectations|matchers|mocks)/
+  ]
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
